bd66dad341abf2ea3e98fd2ee2ce1a2d385361a2
Phy Poh <phypoh@gmail.com>
2023-02-14 16:47:58 +0800

    Added enemy scaling

--- Legion.lua
+++ Legion.lua
@@ -54,49 +54,7 @@ do
 	-- Actions
 	-- =====================================================
 
-	function pack.Parametric.Actions.SpawnEnemy(selection)
-		return function (...)
-			local currentEncounter = CurrentRun.CurrentRoom.Encounter
-			local enemyData = EnemyData[selection]
-			local newEnemy = DeepCopyTable( enemyData )
-			newEnemy.AIOptions = enemyData.AIOptions
-			newEnemy.BlocksLootInteraction = false
-			local invaderSpawnPoint = SelectSpawnPoint(CurrentRun.CurrentRoom, newEnemy, currentEncounter)
-			newEnemy.ObjectId = SpawnUnit({
-					Name = enemyData.Name,
-					Group = "Standing",
-					DestinationId = invaderSpawnPoint, 
-					OffsetX = math.random(-5,5), 
-					OffsetY = math.random(-5,5) 
-				})
-			SetupEnemyObject( newEnemy, CurrentRun )
-			return true
-		end
-	end
-
-	function pack.Parametric.Actions.SpawnEnemies(selection, count)
-		return function (...)
-			local currentEncounter = CurrentRun.CurrentRoom.Encounter
-			local enemyData = EnemyData[selection]
-			for i=1, count do
-				local newEnemy = DeepCopyTable( enemyData )
-				newEnemy.AIOptions = enemyData.AIOptions
-				newEnemy.BlocksLootInteraction = false
-				local invaderSpawnPoint = SelectSpawnPoint(CurrentRun.CurrentRoom, newEnemy, currentEncounter)
-				newEnemy.ObjectId = SpawnUnit({
-						Name = enemyData.Name,
-						Group = "Standing",
-						DestinationId = invaderSpawnPoint, 
-						OffsetX = math.random(-5,5), 
-						OffsetY = math.random(-5,5) 
-					})
-				SetupEnemyObject( newEnemy, CurrentRun )
-				end
-			return true
-		end
-	end
-
-	local function getBossHealthScalingForBiome()
+	local function getHealthScalingForBiome()
 		local biomeNumber = GetBiomeDepth(CurrentRun)
 		-- biomeNumber ==
 		--   Tartarus -> 1 
@@ -106,11 +64,47 @@ do
 		
 		-- scaledHealthMultiplier == 
 		-- 	Tartarus -> 1 
+		
 		-- 	Asphodel -> 1.5
 		-- 	Elysium -> 2
 		-- 	Styx/Surface -> 2.5
+		-- if biomeNumber == nil then
+		-- 	return 1
+		-- end
 
 		return 0.5 * (biomeNumber + 1)
+	end
+
+
+	function SpawnEnemy(enemyData, scaledHealth)
+		local currentEncounter = CurrentRun.CurrentRoom.Encounter
+		local newEnemy = DeepCopyTable( enemyData )
+		if scaledHealth then
+			local newHealth = scaledHealth * getHealthScalingForBiome() 
+			newEnemy.MaxHealth = newHealth
+		end
+		newEnemy.AIOptions = enemyData.AIOptions
+		-- newEnemy.BlocksLootInteraction = false
+		local invaderSpawnPoint = SelectSpawnPoint(CurrentRun.CurrentRoom, newEnemy, currentEncounter)
+		newEnemy.ObjectId = SpawnUnit({
+				Name = enemyData.Name,
+				Group = "Standing",
+				DestinationId = invaderSpawnPoint, 
+				OffsetX = math.random(-200,200), 
+				OffsetY = math.random(-200,200) 
+			})
+		SetupEnemyObject( newEnemy, CurrentRun )
+		return true
+	end
+
+	function pack.Parametric.Actions.SpawnEnemies(selection, count, scaledHealth)
+		return function (...)
+			local enemyData = EnemyData[selection]
+			for i=1, count do
+				SpawnEnemy(enemyData, scaledHealth)
+			end
+		return true
+		end
 	end
 
 	function pack.Parametric.Actions.SpawnBoss(bossName, scaledHealth)
@@ -120,7 +114,7 @@ do
 			local enemyData = EnemyData[bossName]
 			local newEnemy = DeepCopyTable( enemyData )
 			if scaledHealth then
-				local newHealth = scaledHealth * getBossHealthScalingForBiome() 
+				local newHealth = scaledHealth * getHealthScalingForBiome() 
 				newEnemy.MaxHealth = newHealth
 			end
 			newEnemy.AIOptions = enemyData.AIOptions
@@ -140,13 +134,14 @@ do
 	-- =====================================================
 	-- Effects
 	-- =====================================================
-	pack.Effects.SpawnButterflyBall = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemy("FlurrySpawner"))
+
+	pack.Effects.SpawnNumbskull = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("SwarmerHelmeted", 5, 30))
+	pack.Effects.SpawnPest = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("ThiefMineLayer", 5, 40))
+	pack.Effects.SpawnVoidstone = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("ShieldRanged", 1, 175))
+	pack.Effects.SpawnButterflyBall = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("FlurrySpawner", 1, 275))
 	pack.Effects.SpawnFlameWheel = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("ChariotSuicide", 5))
-	pack.Effects.SpawnNumbskull = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("SwarmerHelmeted", 5))
-	pack.Effects.SpawnVoidstone = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemy("ShieldRanged"))
-	pack.Effects.SpawnPest = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("ThiefMineLayer", 5))
-	pack.Effects.SpawnSnakestone = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemy("HeavyRangedForked"))
-	pack.Effects.SpawnSatyr = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemy("SatyrRanged"))
+	pack.Effects.SpawnSnakestone = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("HeavyRangedForked", 1, 215))
+	pack.Effects.SpawnSatyr = cc.BindEffect(pack.Triggers.IfNotFirstRoom, pack.Parametric.Actions.SpawnEnemies("SatyrRanged", 1, 375))
 
 	pack.Effects.SpawnMeg = cc.RigidEffect(cc.BindEffect(pack.Triggers.BossCheck, pack.Parametric.Actions.SpawnBoss( "Harpy", 4400 )))
 	pack.Effects.SpawnAlecto = cc.RigidEffect(cc.BindEffect(pack.Triggers.BossCheck, pack.Parametric.Actions.SpawnBoss( "Harpy2", 4600 )))
@@ -164,7 +159,7 @@ ModUtil.Path.Set( "Legion", ModUtil.Table.Copy( pack.Effects ), cc.Effects )
 -- ModUtil.Path.Wrap( "BeginOpeningCodex", 
 -- 	function(baseFunc)		
 -- 		if not CanOpenCodex() then
--- 			local myfunc = pack.Parametric.Actions.SpawnBoss( "Harpy", 4400 )
+-- 			local myfunc = pack.Parametric.Actions.SpawnEnemies("SwarmerHelmeted", 5, 30)
 -- 			myfunc()
 -- 		end
 -- 		baseFunc()
